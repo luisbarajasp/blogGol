13b9e36892352097b6d26be8f0ecc89fa2af6b66
luisbarajasp <luisbarajasp@gmail.com>
2015-11-08 01:44:21 -0600

    Finish set up for dropbox

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,13 @@
 class Post < ActiveRecord::Base
-	has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "defaultImage.jpg",:storage => :dropbox,
-    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-	validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)
+	if Rails.env.development?
+		has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "defaultImage.jpg"
+		validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)
+	else
+		has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "defaultImage.jpg",:storage => :dropbox,
+    	:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    	:path => ":style/:id_:filename"
+		validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)
+	end
 
 	extend FriendlyId
   	friendly_id :title, use: :slugged
